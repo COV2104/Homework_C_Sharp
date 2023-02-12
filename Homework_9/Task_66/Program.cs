73e302c689e09e7bfbcf2fadf8a253daae546960
COV <tsehlo@mail.ru>
2023-02-12 18:41:41 +0300

    Домашняя работа № 9

---
+++ Homework_9/Task_66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m > n)
+{
+    int temp = m;
+    m = n;
+    n = temp;
+}
+int Sum(int m, int n)
+{
+    if (m == n) return n;
+    return m + Sum(m + 1, n);
+}
+Console.WriteLine($" Сумма чисел от {m} до {n} = {Sum(m, n)}");
---
+++ Homework_9/Task_68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ackerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m != 0) && (n == 0))
+        return Ackerman(m - 1, 1);
+    else
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+}
+
+Console.WriteLine($"Функция Аккермана для чисел M = {m} и N = {n} равна : {Ackerman(m,n)}");
