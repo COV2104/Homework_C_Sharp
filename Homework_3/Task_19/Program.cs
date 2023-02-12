cf33444a154fae97d52a38f4bc8d0985428c2495
COV <tsehlo@mail.ru>
2023-02-12 10:50:23 +0300

    Домашняя работа № 3

---
+++ Homework_3/Task_19/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет; 12821 -> да; 23432 -> да
+
+// 1 вариант:
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int oldNumber = number;
+int reversNumber = 0;
+while (number > 0)
+{
+    int lastDigit = number % 10;
+    reversNumber = reversNumber * 10 + lastDigit;
+    number = number / 10;
+}
+if (oldNumber == reversNumber)
+    Console.WriteLine($"Число {oldNumber} является полиндромом");
+else
+    Console.WriteLine($"Число {oldNumber} не является полиндромом");
+
+// 2 вариант (проверяет число любой размерности):
+
+string Polindrome()
+{
+    Console.Write("Введите число: ");
+    string number = Console.ReadLine();
+    string result = $"Число {number} является полиндром";
+
+    int[] array = new int[number.Length];
+    for (int i = 0; i < number.Length; i++)
+    {
+        array[i] = Convert.ToInt32(number.Substring(i, 1));
+    }
+
+    int[] revers = new int[array.Length];
+    for (int i = 0; i < revers.Length; i++)
+    {
+        revers[i] = array[array.Length - 1 - i];
+    }
+
+    bool find = false;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] != revers[i])
+        {
+            find = true;
+            break;
+        }
+
+    if (find)
+        result = $"Число {number} не является полиндромом";
+    return result;
+}
+try
+{
+    Console.WriteLine(Polindrome());
+}
+catch
+{
+    System.Console.WriteLine("error: Необходимо вводить числа");
+}
---
+++ Homework_3/Task_21/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84; A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// 1 вариант:
+
+Console.Write("Введите координату x точки A: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки A: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки A: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x точки B: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки B: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки B ");
+int zb = Convert.ToInt32(Console.ReadLine());
+double distance = Math.Round(Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2) + Math.Pow((za - zb), 2)),2);
+Console.WriteLine($"Расстояние между точками составляет {distance}");
+
+// 2 вариант:
+
+double DistancePoints()
+{
+    Console.WriteLine("Введите координаты точки А (через запятую): ");
+    int[] coordinatesA = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+    Console.WriteLine("Введите координаты точки B (через запятую): ");
+    int[] coordinatesB = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+
+    double sum = 0;
+    for (int i = 0; i < coordinatesA.Length; i++)
+    {
+        sum = sum + Math.Pow((coordinatesB[i] - coordinatesA[i]), 2);
+    }
+    double distance = Math.Round(Math.Sqrt(sum), 2);
+    return distance;
+}
+try
+{
+    double dist = DistancePoints();
+    Console.WriteLine($"Расстояние между точками составляет {dist}");
+}
+catch
+{
+    System.Console.WriteLine("error: Необходимо вводить цифры");
+}
