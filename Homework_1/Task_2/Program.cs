9db61e71bd3c91083f5493455851a4aa63e76a03
COV <tsehlo@mail.ru>
2023-02-12 10:33:35 +0300

    Домашняя работа № 1

---
+++ Homework_1/Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+
+Console.Write("Введите превое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) max = number2;
+
+Console.WriteLine($"Максимальным из чисел {number1} и {number2} является число {max}");
---
+++ Homework_1/Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+
+Console.Write("Введите превое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.WriteLine($"Максимальным из трех чисел является: {max}");
