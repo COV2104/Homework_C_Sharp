fcdb1acf251c45884578fedc6bac467995d27de5
COV <tsehlo@mail.ru>
2023-02-12 10:56:03 +0300

    Доиашняя работа №4

---
+++ Homework_4/Task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵) 2, 4 -> 16
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int number, int exponent)
+{
+    int i = 1;
+    int stepen = 1;
+    while (i <= exponent)
+    {
+        stepen *= number;
+        i++;
+    }
+    return stepen;
+}
+
+int numExponent = Exponentiation(numberA,numberB);
+Console.WriteLine($"{numberA} ^ {numberB} = {numExponent}");
---
+++ Homework_4/Task_29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// [5] -> [1, 2, 5, 7, 19]; [3] -> [6, 1, 33]
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите элемент массива с индексом [{i}]: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.WriteLine($"{array[i]} ]");
+    }
+}
+
+int[] massiv = FillArray(size);
+PrintArray(massiv);
